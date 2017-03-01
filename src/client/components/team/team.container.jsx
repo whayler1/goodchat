9105b959a068d8e513486c3422c3c9692b291a3a
Justin Worsdale <whayler1@gmail.com>
2017-02-28 21:25:49 -0500

    move team questions into their own component

--- src/client/components/team/team.container.jsx
+++ src/client/components/team/team.container.jsx
@@ -2,9 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import superagent from 'superagent';
-import _ from 'lodash';
-import questionDefaults from '../../questions/questions.js';
 import TeamMemberListItem from './team.member-list-item.component.jsx';
+import TeamQuestions from './team.questions.container.jsx';
 import { setTeam } from './team.dux.js';
 
 import TeamHeader from './team.header.container.jsx';
@@ -22,34 +21,12 @@ class Team extends Component {
   state = {
     name: this.props.team.name || '',
     isNameSet: this.props.team.name ? true : false,
-    question1: this.props.team.question1 || questionDefaults[0][0],
-    question2: this.props.team.question2 || questionDefaults[1][0],
-    question3: this.props.team.question3 || questionDefaults[2][0],
-    question4: this.props.team.question4 || questionDefaults[3][0],
-    question5: this.props.team.question5 || questionDefaults[4][0],
-    isQuestion1DropdownVisible: false,
-    isQuestion2DropdownVisible: false,
-    isQuestion3DropdownVisible: false,
-    isQuestion4DropdownVisible: false,
-    isQuestion5DropdownVisible: false,
-    elRef: null,
     isInFlight: false
   };
 
   resetState = team => this.setState({
     name: team.name || '',
     isNameSet: team.name ? true : false,
-    question1: team.question1 || questionDefaults[0][0],
-    question2: team.question2 || questionDefaults[1][0],
-    question3: team.question3 || questionDefaults[2][0],
-    question4: team.question4 || questionDefaults[3][0],
-    question5: team.question5 || questionDefaults[4][0],
-    isQuestion1DropdownVisible: false,
-    isQuestion2DropdownVisible: false,
-    isQuestion3DropdownVisible: false,
-    isQuestion4DropdownVisible: false,
-    isQuestion5DropdownVisible: false,
-    elRef: null,
     isInFlight: false
   });
 
@@ -63,43 +40,6 @@ class Team extends Component {
       err => console.log('error deleting team', err)
     );
   }
-
-  questionFormSubmit = _.debounce(() => {
-    console.log('%cquestion form submit', 'background:lightblue');
-    const {
-      question1,
-      question2,
-      question3,
-      question4,
-      question5
-    } = this.state;
-
-    superagent.put(`team/${this.props.team.id}`)
-      .send({
-        question1,
-        question2,
-        question3,
-        question4,
-        question5
-      })
-      .end((err, res) => {
-        if (err) {
-          return console.log('%cerr putting team', 'background:pink', res);
-        }
-        console.log('%csuccess updating team!', 'background:yellowgreen', res);
-        this.props.setTeam(res.body.team);
-      });
-  }, 750);
-
-  onQuestionFormSubmit = e => {
-    e.preventDefault();
-
-    this.questionFormSubmit();
-
-    return false;
-  }
-
-  onQuestionChange = e => this.setState({ [e.target.name]: e.target.value }, this.questionFormSubmit);
 
   onNameSubmit = e => {
     e.preventDefault();
@@ -126,22 +66,6 @@ class Team extends Component {
 
   onNameChange = e => this.setState({ name: e.target.value });
 
-  onBodyClick = e => {
-    const { elRef } = this.state;
-    if (elRef && !elRef.contains(e.target)) {
-      this.closeDropdowns();
-    }
-  };
-
-  closeDropdowns = () => this.setState({
-    isQuestion1DropdownVisible: false,
-    isQuestion2DropdownVisible: false,
-    isQuestion3DropdownVisible: false,
-    isQuestion4DropdownVisible: false,
-    isQuestion5DropdownVisible: false,
-    elRef: null
-  });
-
   areQuestionsSet = () => [
     this.props.team.question1,
     this.props.team.question2,
@@ -150,105 +74,6 @@ class Team extends Component {
     this.props.team.question5
   ].every(question => question !== null)
 
-  getQuestionsForm = shouldHavSubmit => {
-    const {
-      question1,
-      question2,
-      question3,
-      question4,
-      question5
-    } = this.props;
-    const questionValues = [
-      question1,
-      question2,
-      question3,
-      question4,
-      question5
-    ];
-
-    return (
-      <form
-        id="team-questions"
-        name="team-questions"
-        className="form"
-        onSubmit={this.onQuestionFormSubmit}
-      >
-        {questionValues.map((question, index) => {
-          const qId = `question${index + 1}`;
-          const elRef = `${qId}El`;
-          const stateName = `isQuestion${index + 1}DropdownVisible`;
-          const onDropdownToggle = () => {
-            const isVisible = this.state[stateName];
-            if (!isVisible) {
-              this.setState({
-                [stateName]: true,
-                elRef: this[elRef]
-              });
-            } else {
-              this.closeDropdowns();
-            }
-          };
-          return (
-            <fieldset>
-              <div className="input-group" ref={el => this[elRef] = el}>
-                <input
-                  id={qId}
-                  name={qId}
-                  type="text"
-                  className="form-control"
-                  value={this.state[qId]}
-                  onChange={this.onQuestionChange}
-                />
-              <span className="input-group-addon input-group-addon-divided">
-                  <button
-                    type="button"
-                    className="btn-no-style"
-                    onClick={onDropdownToggle}
-                  >
-                    <i className="material-icons">keyboard_arrow_down</i>
-                  </button>
-                </span>
-                <div className={`dropdown-container${this.state[stateName] ? ' dropdown-container-show' : ''}`}>
-                  <div className="dropdown">
-                    <ul className="dropdown-list">
-                      {questionDefaults[index].map((question, innerIndex) => {
-                        const onDropdownItemClick = () => this.setState({
-                          [qId]: question,
-                          isQuestion1DropdownVisible: false,
-                          isQuestion2DropdownVisible: false,
-                          isQuestion3DropdownVisible: false,
-                          isQuestion4DropdownVisible: false,
-                          isQuestion5DropdownVisible: false,
-                          elRef: null
-                        }, this.questionFormSubmit);
-                        return (
-                          <li key={`${index}${innerIndex}`}>
-                            <a onClick={onDropdownItemClick}>
-                              {question}
-                            </a>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </fieldset>
-          )
-        })}
-        {shouldHavSubmit &&
-        <fieldset>
-          <button
-            type="submit"
-            className="btn-no-style btn-large team-set-name-submit"
-          >
-            Save questions <i className="material-icons">add_circle_outline</i>
-          </button>
-        </fieldset>}
-      </form>
-    );
-  }
-
   componentWillReceiveProps = nextProps => {
     const { team } = nextProps;
 
@@ -256,18 +81,6 @@ class Team extends Component {
       console.log('nextprops.team.id !== this.props.team.id');
       this.resetState(team);
     }
-  }
-
-  componentDidUpdate = () => {
-    if (this.state.elRef) {
-      document.body.addEventListener('click', this.onBodyClick);
-    } else {
-      document.body.removeEventListener('click', this.onBodyClick);
-    }
-  }
-
-  componentWillUnmount = () => {
-    document.body.removeEventListener('click', this.onBodyClick);
   }
 
   render = () => {
@@ -317,7 +130,10 @@ class Team extends Component {
             <h1 className="vanity-font">Questions</h1>
             <p>What do you want to ask members of <b>{this.state.name}</b>{'?'}</p>
             <p className="main-team-set-questions-footnote">Don't worry, you can continue to update these as your team evolves!</p>
-            {this.getQuestionsForm(true)}
+            <TeamQuestions
+              shouldHaveSubmit={true}
+              team={team}
+            />
             <button
               type="button"
               className="btn-no-style btn-team-skip"
@@ -343,7 +159,9 @@ class Team extends Component {
                     <h3>Questions</h3>
                   </header>
                   <div className="card-padded-content">
-                    {this.getQuestionsForm()}
+                    <TeamQuestions
+                      team={team}
+                    />
                   </div>
                 </section>
               </div>
---
+++ src/client/components/team/team.questions.container.jsx
@@ -0,0 +1,224 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { setTeam } from './team.dux.js';
+import questionDefaults from '../../questions/questions.js';
+import _ from 'lodash';
+import superagent from 'superagent';
+
+class TeamQuestions extends Component {
+  static propTypes = {
+    team: PropTypes.object.isDefined,
+    shouldHaveSubmit: PropTypes.bool,
+    setTeam: PropTypes.func.isRequired
+  };
+
+  state = {
+    question1: this.props.team.question1 || questionDefaults[0][0],
+    question2: this.props.team.question2 || questionDefaults[1][0],
+    question3: this.props.team.question3 || questionDefaults[2][0],
+    question4: this.props.team.question4 || questionDefaults[3][0],
+    question5: this.props.team.question5 || questionDefaults[4][0],
+    isQuestion1DropdownVisible: false,
+    isQuestion2DropdownVisible: false,
+    isQuestion3DropdownVisible: false,
+    isQuestion4DropdownVisible: false,
+    isQuestion5DropdownVisible: false,
+    elRef: null,
+    isInFlight: false
+  };
+
+  resetState = team => this.setState({
+    question1: team.question1 || questionDefaults[0][0],
+    question2: team.question2 || questionDefaults[1][0],
+    question3: team.question3 || questionDefaults[2][0],
+    question4: team.question4 || questionDefaults[3][0],
+    question5: team.question5 || questionDefaults[4][0],
+    isQuestion1DropdownVisible: false,
+    isQuestion2DropdownVisible: false,
+    isQuestion3DropdownVisible: false,
+    isQuestion4DropdownVisible: false,
+    isQuestion5DropdownVisible: false,
+    elRef: null,
+    isInFlight: false
+  });
+
+  questionFormSubmit = _.debounce(() => {
+    console.log('%cquestion form submit', 'background:lightblue');
+    const {
+      question1,
+      question2,
+      question3,
+      question4,
+      question5
+    } = this.state;
+
+    superagent.put(`team/${this.props.team.id}`)
+      .send({
+        question1,
+        question2,
+        question3,
+        question4,
+        question5
+      })
+      .end((err, res) => {
+        if (err) {
+          return console.log('%cerr putting team', 'background:pink', res);
+        }
+        console.log('%csuccess updating team!', 'background:yellowgreen', res);
+        this.props.setTeam(res.body.team);
+      });
+  }, 750);
+
+  onQuestionFormSubmit = e => {
+    e.preventDefault();
+
+    this.questionFormSubmit();
+
+    return false;
+  }
+
+  onQuestionChange = e => this.setState({ [e.target.name]: e.target.value }, this.questionFormSubmit);
+
+  onBodyClick = e => {
+    const { elRef } = this.state;
+    if (elRef && !elRef.contains(e.target)) {
+      this.closeDropdowns();
+    }
+  };
+
+  closeDropdowns = () => this.setState({
+    isQuestion1DropdownVisible: false,
+    isQuestion2DropdownVisible: false,
+    isQuestion3DropdownVisible: false,
+    isQuestion4DropdownVisible: false,
+    isQuestion5DropdownVisible: false,
+    elRef: null
+  });
+
+  componentDidUpdate = () => {
+    if (this.state.elRef) {
+      document.body.addEventListener('click', this.onBodyClick);
+    } else {
+      document.body.removeEventListener('click', this.onBodyClick);
+    }
+  }
+
+  componentWillUnmount = () => {
+    document.body.removeEventListener('click', this.onBodyClick);
+  }
+
+  componentWillReceiveProps = nextProps => {
+    const { team } = nextProps;
+
+    if (team.id !== this.props.team.id) {
+      this.resetState(team);
+    }
+  }
+
+  render() {
+    const {
+      question1,
+      question2,
+      question3,
+      question4,
+      question5,
+      shouldHaveSubmit
+    } = this.props;
+    const questionValues = [
+      question1,
+      question2,
+      question3,
+      question4,
+      question5
+    ];
+
+    return (
+      <form
+        id="team-questions"
+        name="team-questions"
+        className="form"
+        onSubmit={shouldHaveSubmit ? e => {e.preventDefault(); return false;} : this.onQuestionFormSubmit}
+      >
+        {questionValues.map((question, index) => {
+          const qId = `question${index + 1}`;
+          const elRef = `${qId}El`;
+          const stateName = `isQuestion${index + 1}DropdownVisible`;
+          const onDropdownToggle = () => {
+            const isVisible = this.state[stateName];
+            if (!isVisible) {
+              this.setState({
+                [stateName]: true,
+                elRef: this[elRef]
+              });
+            } else {
+              this.closeDropdowns();
+            }
+          };
+          return (
+            <fieldset>
+              <div className="input-group" ref={el => this[elRef] = el}>
+                <input
+                  id={qId}
+                  name={qId}
+                  type="text"
+                  className="form-control"
+                  value={this.state[qId]}
+                  onChange={this.onQuestionChange}
+                />
+              <span className="input-group-addon input-group-addon-divided">
+                  <button
+                    type="button"
+                    className="btn-no-style"
+                    onClick={onDropdownToggle}
+                  >
+                    <i className="material-icons">keyboard_arrow_down</i>
+                  </button>
+                </span>
+                <div className={`dropdown-container${this.state[stateName] ? ' dropdown-container-show' : ''}`}>
+                  <div className="dropdown">
+                    <ul className="dropdown-list">
+                      {questionDefaults[index].map((question, innerIndex) => {
+                        const onDropdownItemClick = () => this.setState({
+                          [qId]: question,
+                          isQuestion1DropdownVisible: false,
+                          isQuestion2DropdownVisible: false,
+                          isQuestion3DropdownVisible: false,
+                          isQuestion4DropdownVisible: false,
+                          isQuestion5DropdownVisible: false,
+                          elRef: null
+                        }, this.questionFormSubmit);
+                        return (
+                          <li key={`${index}${innerIndex}`}>
+                            <a onClick={onDropdownItemClick}>
+                              {question}
+                            </a>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          )
+        })}
+        {shouldHaveSubmit &&
+        <fieldset>
+          <button
+            type="submit"
+            className="btn-no-style btn-large team-set-name-submit"
+          >
+            Save questions <i className="material-icons">add_circle_outline</i>
+          </button>
+        </fieldset>}
+      </form>
+    );
+  }
+}
+
+export default connect(
+  null,
+  {
+    setTeam
+  }
+)(TeamQuestions);
