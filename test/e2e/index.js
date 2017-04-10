52aefeac35c09e16ed70bb85a997df43db34998c
Justin Worsdale <whayler1@gmail.com>
2017-04-10 16:46:09 -0700

    simplest test point to localhost

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,13 +1,17 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Home Page' : function (browser) {
     browser
-      .url('http://www.google.com')
+      .url('http://localhost:3000')
       .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
-      .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
       .end();
+    // browser
+    //   .url('http://www.google.com')
+    //   .waitForElementVisible('body', 1000)
+    //   .setValue('input[type=text]', 'nightwatch')
+    //   .waitForElementVisible('button[name=btnG]', 1000)
+    //   .click('button[name=btnG]')
+    //   .pause(1000)
+    //   .assert.containsText('#main', 'Night Watch')
+    //   .end();
   }
 };
