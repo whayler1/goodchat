2308b7278fcda8ac9863d37cc9eddf5eea5424f6
Justin Worsdale <whayler1@gmail.com>
2017-04-10 17:38:00 -0700

    change window to google auth dialog popup

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,9 +1,27 @@
 module.exports = {
-  'Home Page' : function (browser) {
-    browser
-      .url('http://localhost:3000')
-      .waitForElementVisible('body', 1000)
-      .end();
+  'Test': function (client) {
+    var home = client.page.home();
+    home.navigate()
+      .waitForElementVisible('@loginCta', 1000)
+      .click('@loginCta');
+
+    client.pause(1000);
+    client.window_handles(function(result) {
+      console.log('result:', result);
+      var handle = result.value[1];
+      client.switchWindow(handle, function() {
+        client.waitForElementVisible('body', 1000)
+          .waitForElementVisible('#Email', 10000);
+      });
+    });
+
+    client.end();
+  }
+  // 'Home Page' : function (browser) {
+  //   browser
+  //     .url('http://localhost:3000')
+  //     .waitForElementVisible('body', 1000)
+  //     .end();
     // browser
     //   .url('http://www.google.com')
     //   .waitForElementVisible('body', 1000)
@@ -13,5 +31,5 @@ module.exports = {
     //   .pause(1000)
     //   .assert.containsText('#main', 'Night Watch')
     //   .end();
-  }
+  // }
 };
