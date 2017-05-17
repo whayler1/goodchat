8ec0e33624b9c03515b90b03e9bbe547d356ef80
Justin Worsdale <whayler1@gmail.com>
2017-05-17 10:54:27 -0400

    refactor meeting route to not be modal. Start working out page layout

--- src/client/components/team/team.member-detail.container.jsx
+++ src/client/components/team/team.member-detail.container.jsx
@@ -9,7 +9,6 @@ import { getMeetings } from '../meeting/meeting.dux.js';
 import { updateTeamMembers, createMeeting } from '../team/team.dux.js';
 
 import TeamMemberDetailMeeting from './team.member-detail.meeting.container.jsx';
-import Modal from '../modal/modal.container.jsx';
 import questionDefaults from '../../questions/questions.js';
 import TeamHeader from './team.header.container.jsx';
 import Helmet from 'react-helmet';
@@ -140,129 +139,144 @@ class TeamMemberDetail extends Component {
     const canCreateNewMeeting = meetings.length < 1 || (meetings.length > 0 && meetings[0].is_done);
 
     return (
-      <Modal className="modal-container-team-meeting" closeFunc={this.modalCloseFunc}>
+      <div className="page-layout">
         <Helmet title={`Meetings with ${member.given_name} ${member.family_name} | Good Chat`} />
-        <section className="card">
-          <header className="card-header">
+        <div className="page-row">
+          <header className="header">
             <h3>Meetings with {member.given_name} {member.family_name}</h3>
-            <div className="card-header-close">
-              <Link to={`teams/${team.id}`}>
-                <i className="material-icons">close</i>
-              </Link>
-            </div>
           </header>
-          {canCreateNewMeeting &&
-          <div className="team-card-subheader team-card-subheader-create-meeting">
-            <div className="meeting-header">
-              <div className="meeting-header-lg-icon-wrapper">
-                <i className="material-icons meeting-header-lg-icon">note_add</i>
+        </div>
+        <div className="page-row">
+          <aside className="aside aside-team-meeting">
+            <section>
+              <h3>To-do's</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+          </aside>
+          <main className="main main-team-meeting" role="main">
+            <section className="card">
+              <header className="card-header">
+                <h3>Meetings with {member.given_name} {member.family_name}</h3>
+                <div className="card-header-close">
+                  <Link to={`teams/${team.id}`}>
+                    <i className="material-icons">close</i>
+                  </Link>
+                </div>
+              </header>
+              {canCreateNewMeeting &&
+              <div className="team-card-subheader team-card-subheader-create-meeting">
+                <div className="meeting-header">
+                  <div className="meeting-header-lg-icon-wrapper">
+                    <i className="material-icons meeting-header-lg-icon">note_add</i>
+                  </div>
+                  <div className="meeting-header-lg-content">
+                    <h1 className="meeting-header-title">Create meeting</h1>
+                  </div>
+                </div>
+                <form className="form" onSubmit={this.onSubmit}>
+                  {isScheduleMeetingSelected && [
+                  <fieldset className={newMeetingDateTimeError ? 'input-error' : ''}>
+                    <label
+                      htmlFor="newMeetingDateTime"
+                      className="input-label"
+                    >Meeting date and time</label>
+                    <input
+                      type="datetime-local"
+                      className="form-control"
+                      id="newMeetingDateTime"
+                      name="newMeetingDateTime"
+                      value={newMeetingDateTime}
+                      onChange={this.onChange}
+                    />
+                    {newMeetingDateTimeError &&
+                    <p className="input-error-msg">
+                      {newMeetingDateTimeError === 'doesnt-exist' && 'Please provide a date and time.'}
+                      {newMeetingDateTimeError === 'before-now' && 'New meeting must be in the future.'}
+                    </p>
+                    }
+                  </fieldset>,
+                  <fieldset className="align-right">
+                    <ul className="stacked-to-inline-list">
+                      <li className="hide-handheld">
+                        <button
+                          type="button"
+                          className="btn-no-style btn-no-style-secondary"
+                          onClick={() => this.setState({ isScheduleMeetingSelected: false })}
+                        >Cancel</button>
+                      </li>
+                      <li>
+                        <button
+                          type="submit"
+                          className="btn-primary btn-block"
+                        >Schedule meeting</button>
+                      </li>
+                      <li className="hide-tablet">
+                        <button
+                          type="button"
+                          className="btn-no-style btn-no-style-secondary"
+                          onClick={() => this.setState({ isScheduleMeetingSelected: false })}
+                        >Cancel</button>
+                      </li>
+                    </ul>
+                  </fieldset>]}
+                  {!isScheduleMeetingSelected &&
+                  <fieldset className="align-right">
+                    <ul className="stacked-to-inline-list">
+                      <li>
+                        <button
+                          type="button"
+                          className="btn-primary-inverse btn-block"
+                          onClick={() => this.setState({ newMeetingDateTime: `${moment().add(7, 'd').format('YYYY-MM-DD')}T14:00`, isScheduleMeetingSelected: true })}
+                        >
+                          Schedule meeting
+                        </button>
+                      </li><li>
+                        <button
+                          id="btn-start-meeting-now"
+                          type="button"
+                          className="btn-primary btn-block"
+                          onClick={this.onStartMeetingNow}
+                          autoFocus
+                        >
+                          Start meeting now
+                        </button>
+                      </li>
+                    </ul>
+                  </fieldset>
+                  }
+                </form>
               </div>
-              <div className="meeting-header-lg-content">
-                <h1 className="meeting-header-title">Create meeting</h1>
-              </div>
-            </div>
-            <form className="form" onSubmit={this.onSubmit}>
-              {isScheduleMeetingSelected && [
-              <fieldset className={newMeetingDateTimeError ? 'input-error' : ''}>
-                <label
-                  htmlFor="newMeetingDateTime"
-                  className="input-label"
-                >Meeting date and time</label>
-                <input
-                  type="datetime-local"
-                  className="form-control"
-                  id="newMeetingDateTime"
-                  name="newMeetingDateTime"
-                  value={newMeetingDateTime}
-                  onChange={this.onChange}
-                />
-                {newMeetingDateTimeError &&
-                <p className="input-error-msg">
-                  {newMeetingDateTimeError === 'doesnt-exist' && 'Please provide a date and time.'}
-                  {newMeetingDateTimeError === 'before-now' && 'New meeting must be in the future.'}
-                </p>
-                }
-              </fieldset>,
-              <fieldset className="align-right">
-                <ul className="stacked-to-inline-list">
-                  <li className="hide-handheld">
-                    <button
-                      type="button"
-                      className="btn-no-style btn-no-style-secondary"
-                      onClick={() => this.setState({ isScheduleMeetingSelected: false })}
-                    >Cancel</button>
-                  </li>
-                  <li>
-                    <button
-                      type="submit"
-                      className="btn-primary btn-block"
-                    >Schedule meeting</button>
-                  </li>
-                  <li className="hide-tablet">
-                    <button
-                      type="button"
-                      className="btn-no-style btn-no-style-secondary"
-                      onClick={() => this.setState({ isScheduleMeetingSelected: false })}
-                    >Cancel</button>
-                  </li>
-                </ul>
-              </fieldset>]}
-              {!isScheduleMeetingSelected &&
-              <fieldset className="align-right">
-                <ul className="stacked-to-inline-list">
-                  <li>
-                    <button
-                      type="button"
-                      className="btn-primary-inverse btn-block"
-                      onClick={() => this.setState({ newMeetingDateTime: `${moment().add(7, 'd').format('YYYY-MM-DD')}T14:00`, isScheduleMeetingSelected: true })}
-                    >
-                      Schedule meeting
-                    </button>
-                  </li><li>
-                    <button
-                      id="btn-start-meeting-now"
-                      type="button"
-                      className="btn-primary btn-block"
-                      onClick={this.onStartMeetingNow}
-                      autoFocus
-                    >
-                      Start meeting now
-                    </button>
-                  </li>
-                </ul>
-              </fieldset>
               }
-            </form>
-          </div>
-          }
-          {meetings.length > 0 && !meetings[0].is_done &&
-          <ul className="team-card-body-list">
-            <li key={meetings[0].id}>
-              <TeamMemberDetailMeeting
-                meeting={meetings[0]}
-                todos={todos.filter(todo => todo.meeting_id === meetings[0].id)}
-                imageUrl={imageUrl}
-                memberImageUrl={member.picture}
-                memberId={member.id}
-                history={history}
-              />
-            </li>
-          </ul>}
-        </section>
-        {meetings.length > 1 && <h3 className="meeting-subhead">Previous meetings</h3>}
-        {meetings.filter(meeting => meeting.is_done).map(meeting => (
-        <TeamMemberDetailMeeting
-          key={meeting.id}
-          className="card meeting-card"
-          meeting={meeting}
-          todos={todos.filter(todo => todo.meeting_id === meeting.id)}
-          imageUrl={imageUrl}
-          memberImageUrl={member.picture}
-          memberId={member.id}
-          history={history}
-        />
-        ))}
-      </Modal>
+              {meetings.length > 0 && !meetings[0].is_done &&
+              <ul className="team-card-body-list">
+                <li key={meetings[0].id}>
+                  <TeamMemberDetailMeeting
+                    meeting={meetings[0]}
+                    todos={todos.filter(todo => todo.meeting_id === meetings[0].id)}
+                    imageUrl={imageUrl}
+                    memberImageUrl={member.picture}
+                    memberId={member.id}
+                    history={history}
+                  />
+                </li>
+              </ul>}
+            </section>
+            {meetings.length > 1 && <h3 className="meeting-subhead">Previous meetings</h3>}
+            {meetings.filter(meeting => meeting.is_done).map(meeting => (
+            <TeamMemberDetailMeeting
+              key={meeting.id}
+              className="card meeting-card"
+              meeting={meeting}
+              todos={todos.filter(todo => todo.meeting_id === meeting.id)}
+              imageUrl={imageUrl}
+              memberImageUrl={member.picture}
+              memberId={member.id}
+              history={history}
+            />
+            ))}
+          </main>
+        </div>
+      </div>
     );
   }
 }
