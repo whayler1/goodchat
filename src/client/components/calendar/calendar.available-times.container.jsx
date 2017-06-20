391eaf19cd1c10f8241294bbf2301bf28dc31016
Justin Worsdale <whayler1@gmail.com>
2017-06-19 21:08:50 -0400

    filter events into timeslots that generate calendar ui

--- src/client/components/calendar/calendar.available-times.container.jsx
+++ src/client/components/calendar/calendar.available-times.container.jsx
@@ -11,12 +11,18 @@ class TimeSlot extends Component {
     const { startTime, endTime } = this.props;
 
     return (
-      <button
-        type="button"
-        className="btn btn-block available-times-list-btn"
-      >
-        {startTime.format('h:mmA')}
-      </button>
+      <li>
+        <div className="available-times-list-label">
+          {startTime.format('h:mmA')}
+        </div>
+        <div className="available-times-list-btn-col">
+          <button
+            type="button"
+            className="btn btn-block available-times-list-btn"
+          >
+          </button>
+        </div>
+      </li>
     );
   }
 }
@@ -37,6 +43,10 @@ export default class CalendarAvailableTimes extends Component {
     const { startTime, endTime } = this.props;
     const start = startTime.clone();
 
+    const events = this.props.events.filter(event =>
+      (moment(event.start.dateTime).isAfter(start) && moment(event.start.dateTime).isBefore(endTime)) ||
+      (moment(event.end.dateTime).isAfter(start) && moment(event.end.dateTime).isBefore(endTime)));
+
     const remainder = 30 - start.minute() % 30;
     start.add('minutes', remainder).seconds(0);
 
@@ -45,14 +55,22 @@ export default class CalendarAvailableTimes extends Component {
     const timeSlots = _.times(count, (n) => ({
       startTime: start.clone().add(n * 30, 'minutes'),
       endTime: start.clone().add((n + 1) * 30, 'minutes')
+    })).map(timeSlot => Object.assign(timeSlot, {
+      events: events.filter(event => {
+        const evtStart = moment(event.start.dateTime);
+        const evtEnd = moment(event.end.dateTime);
+        const { startTime, endTime } = timeSlot;
+
+        return (evtStart.isSameOrAfter(startTime) && evtStart.isSameOrBefore(endTime)) ||
+          (evtEnd.isAfter(startTime) && evtEnd.isSameOrBefore(endTime)) ||
+          (evtStart.isBefore(startTime) && evtEnd.isAfter(endTime));
+      })
     }));
 
     const label = startTime.format('MMM DD, YYYY');
 
-    const events = this.props.events.filter(event =>
-      (moment(event.start.dateTime).isAfter(start) && moment(event.start.dateTime).isBefore(endTime)) ||
-      (moment(event.end.dateTime).isAfter(start) && moment(event.end.dateTime).isBefore(endTime)));
     console.log('events', events);
+    console.log('timeSlots', timeSlots);
 
     this.setState({ timeSlots, events, label });
   }
@@ -65,11 +83,10 @@ export default class CalendarAvailableTimes extends Component {
         <span className="input-label">{label}</span>
         <ul className="available-times-list">
           {timeSlots.map((timeSlot, index) => (
-            <li key={index}>
-              <TimeSlot
-                {...timeSlot}
-              />
-            </li>
+            <TimeSlot
+              key={index}
+              {...timeSlot}
+            />
           ))}
         </ul>
       </section>
